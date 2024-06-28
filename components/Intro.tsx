11d523ce83cdbd0fcd2c0606e5aa099553fd2b24
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-28 17:09:35 +0100

    refactor: Optimize Hero and ParalaxHeaderJs components
    - Update Hero component to use lazy loading for images
    - Optimize image loading in ParalaxHeaderJs component
    - Remove unnecessary CSS classes and styles in both components
    - Improve code readability and maintainability

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,16 +1,7 @@
+import { CakeSlice, MessageCircleMore, Cake, PartyPopper } from "lucide-react";
 
-import {
-  BadgeCheck,
-  Cake,
-  CakeSlice,
-  Handshake,
-  MessageCircleMore,
-  PartyPopper,
-  Trophy,
-
-} from "lucide-react";
 import { Button } from "./ui/button";
-import { IntroData } from "./components-data/intro-data";
+import { IntroDataFirst, IntroDataSecond } from "./components-data/intro-data";
 
 
 import React from "react";
@@ -38,21 +29,21 @@ export default function Paralax() {
 
 
       {/* 1 text  */}
-      <section className="">
-        <h3 className="my-8 text-center">Company Values</h3>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 m-10 max-w-5xl mx-auto px-4 md:px-8">
-          {IntroData.map((intro, id) => (
+      <section className="px-4 md:px-8">
+        <h3 className="my-8 lg:text-center">Company Values</h3>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 m-10 max-w-5xl mx-auto">
+          {IntroDataFirst.map((introFirst, id) => (
             <div key={id} className="flex flex-col items-start lg:items-center  justify-start">
               <div className="flex justify-center items-center h-16 w-16 border shadow-inner mr-4 rounded-2xl text-primary/80 dark:text-black dark:bg-white mb-4">
-                {intro.icon}
+                {introFirst.icon}
               </div>
 
               <div className="flex flex-col lg:text-center">
                 <p className="subtitle2 mb-4">
-                  {intro.title}
+                  {introFirst.title}
                 </p>
                 <p className="subtitle3">
-                  {intro.desc}
+                  {introFirst.desc}
                 </p>
               </div>
             </div>
@@ -78,42 +69,22 @@ export default function Paralax() {
       </section>
 
 
+
+
       <div className="flex flex-col gap-y-10 max-w-5xl mx-auto py-12 px-4">
-        <div className="flex items-center">
-          <div className="flex justify-center items-center h-16 w-16 border shadow-inner mr-4 rounded-2xl text-primary/80 dark:text-black dark:bg-white">
-            <Cake className="h-10 w-10 m-3" />
+        {IntroDataSecond.map((introSecond, id) => (
+          <div key={id} className="flex items-center">
+            <div className="flex justify-center items-center h-16 w-16 border shadow-inner mr-4 rounded-2xl text-primary/80 dark:text-black dark:bg-white">
+              {introSecond.icon}
+            </div>
+            <div className="">
+              <p className="subtitle2">
+                {introSecond.title}
+              </p>
+              <p className="subtitle3">{introSecond.desc}</p>
+            </div>
           </div>
-          <div className="">
-            <p className="subtitle2">
-              Discover the perfect cake for every occasion.
-            </p>
-            <p className="subtitle3">From luxurious wedding cakes to delightful birthday surprises, our handcrafted creations are designed to make your celebrations unforgettable.</p>
-          </div>
-        </div>
-
-        <div className="flex items-center">
-          <div className="flex justify-center items-center h-16 w-16 border shadow-inner mr-4 rounded-2xl text-primary/80 dark:text-black dark:bg-white">
-            <PartyPopper className="h-10 w-10 m-3" />
-          </div>
-          <div className="">
-            <p className="subtitle2">
-              Elevate your event with unforgettable moments.
-            </p>
-            <p className="subtitle3">Explore our wide range of flavors, from classic vanilla and rich chocolate to exotic passion fruit and salted caramel. Each cake is a masterpiece, baked with the finest ingredients and decorated with precision and care.</p>
-          </div>
-        </div>
-
-        <div className="flex items-center">
-          <div className="flex justify-center items-center h-16 w-16 border shadow-inner mr-4 rounded-2xl text-primary/80 dark:text-black dark:bg-white">
-            <PartyPopper className="h-10 w-10 m-3" />
-          </div>
-          <div className="">
-            <p className="subtitle2">
-              Custom Cake Designs
-            </p>
-            <p className="subtitle3"> Can&apos;t find exactly what you&apos;re looking for? Let us create a custom cake that perfectly matches your vision. From the initial sketch to the final decoration, we work closely with you to bring your dream cake to life.</p>
-          </div>
-        </div>
+        ))}
 
         <div className="flex flex-col items-center justify-center">
           <Link href="/contact">
@@ -131,46 +102,23 @@ export default function Paralax() {
         {/* exterior paralax */}
         <div className="py-[20%] px-0">
           {/* inner paralax */}
-          <h1 className="text-5xl sm:text-7xl md:text-9xl text-center text-white/60">Customer Satisfaction:</h1>
+          <h1 className="text-5xl sm:text-7xl md:text-9xl text-center text-white/60">Customer Satisfaction</h1>
         </div>
       </section>
 
+      <section className="flex flex-col max-w-5xl mx-auto px-4 md:px-8 lg:text-center mb-8">
+        <h3 className="my-8">Custom Cake Designs</h3>
+        <p className="subtitle3 mb-8">
+          Can&apos;t find exactly what you&apos;re looking for? Let us create a custom cake that perfectly matches your vision. From the initial sketch to the final decoration, we work closely with you to bring your dream cake to life.
+        </p>
 
-      Customer Satisfaction:
-      <h4>Custom Cake Designs</h4>
-      <p className="text-base text-gray-500 text-justify leading-8 mx-12 my-10">
-        Can&apos;t find exactly what you&apos;re looking for? Let us create a custom cake that perfectly matches your vision. From the initial sketch to the final decoration, we work closely with you to bring your dream cake to life.
-      </p>
-
-
+      </section>
 
     </div>
   );
 }
 
 
-
-
-{/* <section className="mx-8">
-        <div className="max-w-7xl my-14 md:my-20 mx-auto">
-          <h2 className="text-center mb-8 md:mb-12 text-gray-800 dark:text-primary max-w-[550px] mx-auto">
-            My Services:
-          </h2>
-          <h4 className="md:mb-4 text-center my-4 md:my-12 light:text-gray-800 border-b border-primary max-w-[250px] mx-auto md:mx-0">
-            Our Specialties
-          </h4>
-          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto text-center md:text-left">
-            {plumbingData.map((plumbingService, id) => (
-              <li
-                key={id}
-                className="list-disc list-inside text-wrap text-sm md:text-base m-0 p-0"
-              >
-                {plumbingService.plumbing}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section> */}
 
 
 
