95f2c7fe8d8f971be669f69cacdd27a233d7a57c
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-07-02 09:56:25 +0100

    Update BounceStars component styles for stars

--- components/BounceStars.tsx
+++ components/BounceStars.tsx
@@ -3,13 +3,12 @@ import { Star, StarHalf } from 'lucide-react'
 export default function BounceStars() {
   return (
 
-    <div className="flex items-center justify-center my-5 text-primary dark:text-orange-500">
-      <Star className='animate-bounce transition-all shadow-md dark:shadow-inner rounded-full' />
-      <Star className='animate-bounce transition-all shadow-md dark:shadow-inner rounded-full delay-150' />
-      <Star className='animate-bounce transition-all shadow-md dark:shadow-inner rounded-full delay-300' />
-      <Star className='animate-bounce transition-all shadow-md dark:shadow-inner rounded-full delay-500' />
-      <Star className='animate-bounce transition-all shadow-md dark:shadow-inner rounded-full delay-700' />
+    <div className="flex items-center justify-center my-5 text-primary dark:text-orange-300">
+      <Star className='animate-bounce transition-all rounded-full' />
+      <Star className='animate-bounce transition-all rounded-full delay-150' />
+      <Star className='animate-bounce transition-all rounded-full delay-300' />
+      <Star className='animate-bounce transition-all rounded-full delay-500' />
+      <Star className='animate-bounce transition-all rounded-full delay-700' />
     </div>
-
   )
 }
